jjpprrrr/device_xiaomi_sdm845-common

sdm845-common: Switch to use stable aidl power HAL
Bug: 154570109
Test: build and capture trace to check
Test: VtsHalPowerTargetTest passed
Change-Id: Ifa022f42014cc1743a07be3a42eaa0d52567361f
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

sdm845.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi-libperfmgr \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \

TheNotOnly/android_device_lge_sdm845-common

sdm845.mk
@@ -374,8 +374,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.lge-libperfmgr \
-    android.hardware.power.stats@1.0-service.lge
+    android.hardware.power-service.lge-libperfmgr \
+    android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json