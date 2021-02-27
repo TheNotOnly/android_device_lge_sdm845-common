jjpprrrr/device_xiaomi_sdm845-common

sdm845-common: Power: rename namespace for Pixel power HAL
Bug: 152811907
Test: Build
Change-Id: I6848e929d8a26b540fcee9145376b896c3cd9799
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

power-libperfmgr/aidl/service.cpp
@@ -26,8 +26,8 @@
 #include "Power.h"
 #include "PowerExt.h"
 
-using aidl::android::hardware::xiaomi::extension::power::impl::PowerExt;
-using aidl::android::hardware::power::impl::xiaomi::Power;
+using aidl::google::hardware::power::impl::xiaomi::Power;
+using aidl::google::hardware::power::impl::xiaomi::PowerExt;
 using ::android::perfmgr::HintManager;
 
 constexpr char kPowerHalConfigPath[] = "/vendor/etc/powerhint.json";

TheNotOnly/android_device_lge_sdm845-common

power-libperfmgr/aidl/service.cpp
@@ -26,8 +26,8 @@
 #include "Power.h"
 #include "PowerExt.h"
 
-using aidl::android::hardware::lge::extension::power::impl::PowerExt;
-using aidl::android::hardware::power::impl::lge::Power;
+using aidl::google::hardware::power::impl::lge::Power;
+using aidl::google::hardware::power::impl::lge::PowerExt;
 using ::android::perfmgr::HintManager;
 
 constexpr char kPowerHalConfigPath[] = "/vendor/etc/powerhint.json";